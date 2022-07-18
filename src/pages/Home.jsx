87ab21c7928dbb6836012ce39212db2ef64db628
Seona <suna9267@gmail.com>
2022-07-18 19:11:01 +0900

    홈화면 카드, 이미지에 css 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,7 +27,9 @@ const Home = () => {
                 <div className={cx("text_box")}>
                   WHY DO YOU MAKE EFFORTS COMMONLY, DON'T WANT TO LIVE COMMONLY!
                 </div>
-                <div className={cx("author")}>- John.F.Kennedy -</div>
+                <div className={cx("info")}>
+                  <h3>John.F.Kennedy</h3>
+                </div>
               </div>
               <div id={styles["second_box"]} className={cx("sentence_box")}>
                 <div className={cx("text_box")}>
@@ -37,7 +39,9 @@ const Home = () => {
                   진심으로 별을 바라보거나 기타 소리에 미친 듯이 끌려들거나 하는
                   시기란 인생에서 극히 잠깐밖에 없으며, 그것은 아주 좋은 것이다.
                 </div>
-                <div className={cx("author")}>- 책제목 -</div>
+                <div className={cx("info")}>
+                  <h3>책 제목</h3>
+                </div>
               </div>
               <div id={styles["third_box"]} className={cx("sentence_box")}>
                 <div className={cx("text_box")}>
@@ -45,7 +49,9 @@ const Home = () => {
                   마음 깊숙이 꽂힌 언어는 지지 않는 꽃입니다. 우린 그 꽃을
                   바라보며 위안을 얻기도 합니다.
                 </div>
-                <div className={cx("author")}>- 언어의 온도 -</div>
+                <div className={cx("info")}>
+                  <h3>언어의 온도</h3>
+                </div>
               </div>
               <div id={styles["forth_box"]} className={cx("sentence_box")}>
                 <div className={cx("text_box")}>
@@ -53,8 +59,8 @@ const Home = () => {
                   시간은 그간 우리가 지나온 익숙함들을 가장 눈부신 것으로
                   되돌려놓는다. 떠나야 돌아올 수 있다.
                 </div>
-                <div className={cx("author")}>
-                  - 운다고 달라지는 일은 아무것도 없겠지만 -
+                <div className={cx("info")}>
+                  <h3>운다고 달라지는 일은 아무것도 없겠지만</h3>
                 </div>
               </div>
             </div>
@@ -74,6 +80,9 @@ const Home = () => {
                     className={cx("sentence_image")}
                     alt="sentence_image01"
                   />
+                  <div className={cx("info")}>
+                    <h3>운다고 달라지는 일은 아무것도 없겠지만</h3>
+                  </div>
                 </div>
               </div>
               <div id={styles["second_image"]} className={cx("image_box")}>
@@ -83,6 +92,9 @@ const Home = () => {
                     className={cx("sentence_image")}
                     alt="sentence_image01"
                   />
+                  <div className={cx("info")}>
+                    <h3>운다고 달라지는 일은 아무것도 없겠지만</h3>
+                  </div>
                 </div>
               </div>
               <div id={styles["third_image"]} className={cx("image_box")}>
@@ -92,6 +104,9 @@ const Home = () => {
                     className={cx("sentence_image")}
                     alt="sentence_image01"
                   />
+                  <div className={cx("info")}>
+                    <h3>운다고 달라지는 일은 아무것도 없겠지만</h3>
+                  </div>
                 </div>
               </div>
               <div id={styles["fourth_image"]} className={cx("image_box")}>
@@ -101,6 +116,9 @@ const Home = () => {
                     className={cx("sentence_image")}
                     alt="sentence_image01"
                   />
+                  <div className={cx("info")}>
+                    <h3>운다고 달라지는 일은 아무것도 없겠지만</h3>
+                  </div>
                 </div>
               </div>
             </div>
@@ -114,7 +132,7 @@ const Home = () => {
               />
               <br />
               <p className={cx("book_name-container")}>
-                <div className={cx("book_name")}>자전거여행2</div>
+                <div className={cx("book_name")}>자전거여행</div>
                 <div className={cx("book_author")}>김훈</div>
               </p>
             </div>
